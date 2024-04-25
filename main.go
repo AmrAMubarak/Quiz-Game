e4219390916cffe9adbd180ee6fa1df3008809ab
Amr Mubarak <138404703+AmrAMubarak@users.noreply.github.com>
2024-04-25 11:42:20 +0200

    Update main.go

--- main.go
+++ main.go
@@ -4,35 +4,54 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"time"
 )
 
 const problemsFileName = "problem.csv"
 
+var (
+	correctAnswers int
+)
+
 func main() {
+
+	questions := readCSVFile(problemsFileName)
+
+	go StartQuiz(questions)
+
+	startTimer(20 * time.Second)
+
+	fmt.Printf("Result: %d / %d\n", correctAnswers, len(questions))
+}
+
+func readCSVFile(problemsFileName string) [][]string {
 	file, err := os.Open(problemsFileName)
+
 	if err != nil {
 		fmt.Printf("failed to open the file: %v \n", err)
-		return
+		panic(err)
 	}
 
 	defer file.Close()
+
 	r := csv.NewReader(file)
-
 	questions, err := r.ReadAll()
-	if err != nil {
-		fmt.Printf("Something want wrong %v", err)
-		return
-	}
 
-	var correctAnswers int
-	for i, record := range questions {
+	if err != nil {
+		fmt.Printf("failed to read csv file:  %v", err)
+		panic(err)
+	}
+	return questions
+}
+
+func StartQuiz(questions [][]string) {
+
+	for index, record := range questions {
 		question, correctAnswer := record[0], record[1]
 
-		//display one question at a time
-		fmt.Printf("%d. %s ? \n", i+1, question)
-
+		fmt.Printf("%d. %s ? \n", index+1, question)
 		var asnwer string
-		// get answer from user, thenm proceed to next question immediately
+
 		if _, err := fmt.Scan(&asnwer); err != nil {
 			fmt.Printf("failed to  scan: %v", err)
 			return
@@ -41,6 +60,10 @@ func main() {
 			correctAnswers++
 		}
 	}
-	// output the total number correct and how many
 	fmt.Printf("Result: %d / %d\n", correctAnswers, len(questions))
+	os.Exit(0)
+}
+
+func startTimer(duration time.Duration) {
+	time.Sleep(duration)
 }
