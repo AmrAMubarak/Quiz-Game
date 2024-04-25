f0fce672a94d72f197b0b545a0cb273528ef2f17
Amr Mubarak <138404703+AmrAMubarak@users.noreply.github.com>
2024-04-25 18:05:18 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,34 +2,35 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
 	"time"
 )
 
-const problemsFileName = "problem.csv"
-
 var (
-	correctAnswers int
+	problemsFileName *string
+	correctAnswers   int
 )
 
 func main() {
-
+	problemsFileName = flag.String("csv", "problem.csv", "a csv file in the format of 'question,asnwer'")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	flag.Parse()
 	questions := readCSVFile(problemsFileName)
 
 	go StartQuiz(questions)
 
-	startTimer(20 * time.Second)
+	startTimer(time.Duration(*timeLimit) * time.Second)
 
-	fmt.Printf("Result: %d / %d\n", correctAnswers, len(questions))
+	fmt.Printf("\nResult: %d / %d\n", correctAnswers, len(questions))
 }
 
-func readCSVFile(problemsFileName string) [][]string {
-	file, err := os.Open(problemsFileName)
+func readCSVFile(problemsFileName *string) [][]string {
+	file, err := os.Open(*problemsFileName)
 
 	if err != nil {
-		fmt.Printf("failed to open the file: %v \n", err)
-		panic(err)
+		exit(fmt.Sprintf("failed to open the file: %v \n", *problemsFileName))
 	}
 
 	defer file.Close()
@@ -38,8 +39,7 @@ func readCSVFile(problemsFileName string) [][]string {
 	questions, err := r.ReadAll()
 
 	if err != nil {
-		fmt.Printf("failed to read csv file:  %v", err)
-		panic(err)
+		exit(fmt.Sprintf("failed to read csv file:  %v", *problemsFileName))
 	}
 	return questions
 }
@@ -49,7 +49,8 @@ func StartQuiz(questions [][]string) {
 	for index, record := range questions {
 		question, correctAnswer := record[0], record[1]
 
-		fmt.Printf("%d. %s ? \n", index+1, question)
+		fmt.Printf("Problem #%d: %s = ", index+1, question)
+
 		var asnwer string
 
 		if _, err := fmt.Scan(&asnwer); err != nil {
@@ -66,4 +67,8 @@ func StartQuiz(questions [][]string) {
 
 func startTimer(duration time.Duration) {
 	time.Sleep(duration)
+}
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
